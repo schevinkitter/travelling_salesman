31d04ea92e1b6fce6c1ff6ffa896d5954d89596c
Kevin Schitter <k.schitter@student.tugraz.at>
2024-02-08 16:12:33 +0100

    added animation functions, also a function to read external .tsp data

--- tsp_utils.py
+++ tsp_utils.py
@@ -1,8 +1,10 @@
 import numpy as np
 import random
+import matplotlib.pyplot as plt
+from matplotlib.animation import FuncAnimation
 
 """
-Tools for solving the tsp problem
+Toolbox for solving the tsp problem
 """
 
 
@@ -15,33 +17,137 @@ def distance_matrix(coords):
     return d_ij
 
 
-def generate_rand_cities(N):
-    
-        x = np.random.rand(N)
-        y = np.random.rand(N)
-        return np.column_stack((x, y))
-    
-    
+def generate_rand_cities(N: int):
+    """Create random city locations in 2D
+
+    Args:
+        N (int): number of random cities
+
+    Returns:
+        np.ndarray: x and y coordinates as columns
+    """
+    x = np.random.rand(N)
+    y = np.random.rand(N)
+    return np.column_stack((x, y))
+
+
 def get_nearest_neighbour_solution(distance_matrix):
-    
+    """Orders cities initially after nearest neighbour distances
+    """
+
     d_ij = distance_matrix
-    
     # choose random city to start
     city = random.randrange(len(d_ij))
-    result = [city]
-    
+    nn_ordering = [city]
+
     # list of cities which should be visited
     cities_to_visit = list(range(len(d_ij)))
     cities_to_visit.remove(city)
-    
-    # visit all cities and find the nearest
+
+    # look for the next neares city
     while cities_to_visit:
         # generate touples (distance, #city) and always look for the one with smalles distance
-        nearest_city = min([(d_ij[city][j], j) for j in cities_to_visit], key=lambda x:x[0])
+        nearest_city = min([(d_ij[city][j], j)
+                           for j in cities_to_visit], key=lambda x: x[0])
         city = nearest_city[1]
         cities_to_visit.remove(city)
-        result.append(city)
-    
-    
-    return result
-    
+        nn_ordering.append(city)
+
+    return nn_ordering
+
+
+def get_rand_solution(distance_matrix):
+    # just orders the randomly generated coordinates from 1 up to the number of cities
+    return [i for i in range(len(distance_matrix))]
+
+
+def load_tsp_problem(path: str):
+    """Can be used to load existing tsp problems with 'known' solution as .tsp files. 
+
+    Args:
+        path (str): file path of the .tsp file
+
+    Returns:
+        initial_tour: coordinate array of cities
+    """
+    import tsplib95
+    data = tsplib95.load(path)
+
+    node_coords = data.node_coords
+
+    x = np.array([c[0] for c in node_coords.values()])
+    y = np.array([c[1] for c in node_coords.values()])
+    initial_tour = np.column_stack((x, y))
+
+    return initial_tour
+
+
+def animation_tsp(history, city_locations):
+
+    fig, ax = plt.subplots()
+
+    line, = plt.plot([], [], lw=2)
+
+    def init():
+        x, y = city_locations.T
+        plt.plot(x, y, 'o', color='black')
+
+        line.set_data([], [])
+
+        return line,
+
+    def update(frame):
+        x = [city_locations[i, 0]
+             for i in history[frame] + [history[frame][0]]]
+        y = [city_locations[i, 1]
+             for i in history[frame] + [history[frame][0]]]
+        line.set_data(x, y)
+        return line
+
+    ani = FuncAnimation(fig, update, frames=range(0, len(history), len(history)//100),
+                        init_func=init, interval=60, repeat=False)
+
+    plt.show()
+
+    return
+
+
+def animation_tsp_v2(history, history_H, city_locations):
+
+    fig, (ax1, ax2) = plt.subplots(1, 2, figsize=(10,5))
+
+    line1, = ax1.plot([], [], lw=2)
+    line2, = ax2.plot([], [], lw=2)
+
+    def init():
+        x, y = city_locations.T
+        h, iters = history_H, [i for i in range(len(history_H))]
+
+        ax1.plot(x, y, 'o', color='black')
+
+
+        ax2.set_xlim(0, len(history_H))
+        ax2.set_ylim(0, max(history_H) + 0.1*max(history_H))
+
+        line1.set_data([], [])
+        line2.set_data([], [])
+        return line1, line2
+
+    def update(frame):
+        x = [city_locations[i, 0]
+             for i in history[frame] + [history[frame][0]]]
+        y = [city_locations[i, 1]
+             for i in history[frame] + [history[frame][0]]]
+        
+        h = history_H[:frame]
+        iters = list(range(frame))
+        line1.set_data(x, y)
+        line2.set_data(iters, h)
+        return line1, line2
+
+    ani = FuncAnimation(fig, update, frames=range(0, len(history), len(history)//100),
+                        init_func=init, interval=60, repeat=False)
+
+    plt.show()
+
+    return
