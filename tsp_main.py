1fb13a91568dd8e6e3aa61c36b356bd1c0f265f3
Kevin Schitter <k.schitter@student.tugraz.at>
2024-02-08 16:13:31 +0100

    added external data problem for illustration

--- tsp_main.py
+++ tsp_main.py
@@ -1,24 +1,31 @@
 import numpy as np
 from annealing import Annealing
-from tsp_utils import generate_rand_cities
+from tsp_utils import generate_rand_cities, load_tsp_problem
 
 
 def main():
 
-    coords = generate_rand_cities(10)
+    # coords = generate_rand_cities(50)
+    coords = load_tsp_problem('data/pr124.tsp')
 
     tsm1 = Annealing(coordinates=coords,
-                     betaminmax=[0.001, 50],
-                     alpha=1.1,
-                     maxiter=10_000)
-    
+                     betaminmax=[0.001, 400000],
+                     alpha=1.001,
+                     maxiter=100_000)
+
     tsm1.anneal()
-    
     tsm1.plot_energy_hist()
-    
     tsm1.animate_solution()
 
+    # tsm2 = Annealing(coordinates=coords,
+    #                  betaminmax=[0.1, 400000],
+    #                  alpha=1.01,
+    #                  maxiter=100_000)
 
+    # tsm2.anneal()
+
+    # # tsm2.plot_energy_hist()
+    # tsm2.animate_solution()
 
 
 if __name__ == '__main__':
